84741c01d4e7fd66ee4e3f633297703899d27cc0
lianghua1987 <hua_liang@mckinsey.com>
2023-10-17 13:59:37 +0800

    Finish rest api

--- books.py
+++ books.py
@@ -1,7 +1,8 @@
 from typing import Optional
 
-from fastapi import Body, FastAPI
+from fastapi import Body, FastAPI, Path, Query, HTTPException
 from pydantic import BaseModel, Field
+from starlette import status
 
 app = FastAPI()
 
@@ -37,27 +38,45 @@ class BookRequest(BaseModel):
     description: str = Field(min_length=3, max_length=200)
     rating: int = Field(gt=-1, lt=6)
 
+    class Config:
+        json_schema_extra = {
+            'example': {
+                'title': 'A new book',
+                'author': 'tester',
+                'description': 'A book desc',
+                'rating': 5
+            }
+        }
 
-@app.get("/books")
+
+@app.get("/books", status_code=status.HTTP_200_OK)
 async def all_books():
     return BOOKS
 
 
-@app.get("/books/{title}")
+@app.get("/books/{id}", status_code=status.HTTP_200_OK)
+async def read_book(id: int = Path(gt=0)):
+    books = [book for book in BOOKS if book.id == id]
+    if len(books) == 0:
+        raise HTTPException(status_code=404, detail="Item not found")
+    return books[0]
+
+
+@app.get("/books/", status_code=status.HTTP_200_OK)
+async def books_by_rating(rating: int = Query(gt=0, lt=6)):
+    return [book for book in BOOKS if book.rating == rating]
+
+
+@app.get("/books/{title}", status_code=status.HTTP_200_OK)
 async def books_by_title(title: str):
     return [book for book in BOOKS if book.title == title]
-
-
-@app.get("/books/")
-async def books_by_title(title: str):
-    return [book for book in BOOKS if book.title == title.casefold()]
 
 
 def find_max_id():
     return 1 if len(BOOKS) == 0 else BOOKS[-1].id
 
 
-@app.post("/books/create")
+@app.post("/books/create", status_code=status.HTTP_201_CREATED)
 async def add_book(book_request: BookRequest):
     new_book = Book(**book_request.model_dump())
     new_book.id = find_max_id() + 1
@@ -65,18 +84,24 @@ async def add_book(book_request: BookRequest):
     return BOOKS
 
 
-@app.put("/books/update")
+@app.put("/books/update", status_code=status.HTTP_204_NO_CONTENT)
 async def update_book(book_request: BookRequest):
+    changed = False
     updated_book = Book(**book_request.model_dump())
     for i in range(len(BOOKS)):
         if BOOKS[i].title.casefold() == updated_book.title.casefold():
             BOOKS[i] = updated_book
-    return BOOKS
+            changed = True
+    if not changed:
+        raise HTTPException(status_code=404, detail='Item not found')
 
 
-@app.delete("/books/delete/{title}")
+@app.delete("/books/delete/{title}", status_code=status.HTTP_204_NO_CONTENT)
 async def add_book(title: str):
+    deleted = False
     for i in range(len(BOOKS)):
         if BOOKS[i].title.casefold() == title.casefold():
             BOOKS.pop(i)
-    return BOOKS
+            deleted = True
+    if not deleted:
+        raise HTTPException(status_code=404, detail='Item not found')
